1d4e5060105828e587fb7b66b74a580ea5e5e1f3
chadcwilliams <ccwillia@uvic.ca>
2021-05-31 20:38:35 -0700

    Quick fix of bug brought up by last update

--- StatsProblemGenerator/app.R
+++ StatsProblemGenerator/app.R
@@ -38,7 +38,7 @@ ui = fluidPage(tags$head(tags$style(type = "text/css", ".irs {max-width: 946px;}
                        sliderInput(
                            inputId = 'value_range',
                            label = 'Range of Values',
-                           value = c(1,10),
+                           value = c(1, 10),
                            min = 1,
                            max = 100,
                            step = 1,
@@ -48,13 +48,14 @@ ui = fluidPage(tags$head(tags$style(type = "text/css", ".irs {max-width: 946px;}
                        actionButton('distribution', 'Plot Data'),
                        actionButton('answers', 'Show Answers')
                    ),
-                   mainPanel(
-                       fluidRow(column(3, rHandsontableOutput("data_display")),
-                                column(9, plotOutput('distribution_display'))),
-                       fluidRow(column(12,rHandsontableOutput("stats_display")
-                       )
-                   )
-               )))
+                   mainPanel(fluidRow(
+                       column(3, rHandsontableOutput("data_display")),
+                       column(9, plotOutput('distribution_display'))
+                   ),
+                   fluidRow(column(
+                       12, rHandsontableOutput("stats_display")
+                   )))
+               ))
 
 #Server
 server = function(input, output) {
@@ -64,7 +65,11 @@ server = function(input, output) {
         #Frequency Distribution
         if (input$Test == 1) {
             #Create Data
-            data = sample(input$value_range[1]:input$value_range[2], input$num_of_participants, replace = TRUE)
+            data = sample(
+                input$value_range[1]:input$value_range[2],
+                input$num_of_participants,
+                replace = TRUE
+            )
             plotdata$data = as.data.frame(data)
             #Create Stats
             freq_dist = table(data)
@@ -86,7 +91,7 @@ server = function(input, output) {
                 Cum_Rel_Freq = cumsum(rel_freq)
             )
             for (counter in 1:dim(freq_dist)[1]) {
-                frequency_distribution[frequency_distribution$Data == frequency_dist$Data[counter], ] = frequency_dist[counter, ]
+                frequency_distribution[frequency_distribution$Data == frequency_dist$Data[counter],] = frequency_dist[counter,]
             }
             
             missing = which(is.na(frequency_distribution$Frequency))
@@ -100,17 +105,23 @@ server = function(input, output) {
             }
             frequency_distribution$Frequency = as.integer(frequency_distribution$Frequency)
             #Set outputs
-            stats$data_table = frequency_distribution[order(nrow(frequency_distribution):1), ]
+            stats$data_table = frequency_distribution[order(nrow(frequency_distribution):1),]
             output$data_display = renderRHandsontable(rhandsontable(as.data.frame(data)))
             output$stats_display = renderRHandsontable({
+                
             })
             output$distribution_display = renderPlot({
+                
             })
         }
         else if (input$Test == 2) {
             #Descriptives
             #Create Data
-            data = sample(input$value_range[1]:input$value_range[2], input$num_of_participants, replace = TRUE)
+            data = sample(
+                input$value_range[1]:input$value_range[2],
+                input$num_of_participants,
+                replace = TRUE
+            )
             plotdata$data = as.data.frame(data)
             #Force a Mode (By duplicating one of the numbers)
             if (length(unique(data)) == input$num_of_participants) {
@@ -124,16 +135,19 @@ server = function(input, output) {
                 unique_x[tabulate_x == max(tabulate_x)]
             }
             #Setup Semi-Interquartile Range Function
-            siqr = function(data){
+            siqr = function(data) {
                 sorted_data = sort(data)
-                if (length(sorted_data)%%2==0){
-                    q1 = median(sorted_data[1:(length(sorted_data)/2)])
-                    q3 = median(sorted_data[((length(sorted_data)/2)+1):length(sorted_data)])
-                }else{
-                    q1 = median(sorted_data[1:((length(sorted_data)-1)/2)])
-                    q3 = median(sorted_data[((length(sorted_data)-((length(sorted_data)-1)/2))+1):length(sorted_data)])
+                if (length(sorted_data) %% 2 == 0) {
+                    q1 = median(sorted_data[1:(length(sorted_data) / 2)])
+                    q3 = median(sorted_data[((length(sorted_data) / 2) +
+                                                 1):length(sorted_data)])
+                } else{
+                    q1 = median(sorted_data[1:((length(sorted_data) - 1) / 2)])
+                    q3 = median(sorted_data[((length(sorted_data) - ((
+                        length(sorted_data) - 1
+                    ) / 2)) + 1):length(sorted_data)])
                 }
-                (q3-q1)/2
+                (q3 - q1) / 2
             }
             #Create Stats
             descriptives = data_table = data.frame(
@@ -144,9 +158,13 @@ server = function(input, output) {
                 SIQR = siqr(data),
                 MAD = median(abs(data - median(data))),
                 SS = sum((data - mean(data)) ^ 2),
-                Var = sum((data - mean(data)) ^ 2)/input$num_of_participants,
-                SD = sqrt(sum((data - mean(data)) ^ 2)/input$num_of_participants),
-                SkewP = (3*(mean(data)-median(data)))/sqrt(sum((data - mean(data)) ^ 2)/input$num_of_participants)
+                Var = sum((data - mean(data)) ^ 2) / input$num_of_participants,
+                SD = sqrt(sum((
+                    data - mean(data)
+                ) ^ 2) / input$num_of_participants),
+                SkewP = (3 * (mean(data) - median(data))) / sqrt(sum((
+                    data - mean(data)
+                ) ^ 2) / input$num_of_participants)
             )
             #Clear the Duplicate Values
             if (dim(descriptives)[1] > 1) {
@@ -159,24 +177,45 @@ server = function(input, output) {
                 
             })
             output$distribution_display = renderPlot({
+                
             })
         }
         else if (input$Test == 3) {
-            #Single Sample Z-Test
+            #Single Participant Z-Test
             #Create Data
             data = data.frame(
-                X = sample(seq(input$value_range[1],input$value_range[2],by=.1), 1),
-                mu = sample(seq(input$value_range[1],input$value_range[2],by=.1), 1),
-                sigma = rnorm(1,(input$value_range[2]-input$value_range[1])/5, .1))
-            data2 = data.frame(
-                data = dnorm(seq((data$mu-(4*data$sigma)),(data$mu+(4*data$sigma)),length.out = 100), mean = data$mu, sd = data$sigma))
+                X = sample(
+                    seq(input$value_range[1], input$value_range[2], by = .1),
+                    1
+                ),
+                mu = sample(
+                    seq(input$value_range[1], input$value_range[2], by = .1),
+                    1
+                ),
+                sigma = rnorm(1, (
+                    input$value_range[2] - input$value_range[1]
+                ) / 5, .1)
+            )
+            data2 = data.frame(data = dnorm(
+                seq((data$mu - (
+                    4 * data$sigma
+                )), (data$mu + (
+                    4 * data$sigma
+                )), length.out = 100),
+                mean = data$mu,
+                sd = data$sigma
+            ))
             plotdata$data = data2
             
             #Create Table
             descriptives = data.frame(
-                Z_Value = (data$X-data$mu)/data$sigma,
-                P_Value_of_X_and_Below =round(pnorm(round((data$X-data$mu)/data$sigma,digits =2)),digits=4),
-                P_Value_of_X_and_Above = round(pnorm(round((data$X-data$mu)/data$sigma,digits =2),lower.tail = F),digits=4)
+                Z_Value = (data$X - data$mu) / data$sigma,
+                P_Value_of_X_and_Below = round(pnorm(
+                    round((data$X - data$mu) / data$sigma, digits = 2)
+                ), digits = 4),
+                P_Value_of_X_and_Above = round(pnorm(
+                    round((data$X - data$mu) / data$sigma, digits = 2), lower.tail = F
+                ), digits = 4)
             )
             #Set Outputs
             stats$data_table = descriptives
@@ -185,13 +224,65 @@ server = function(input, output) {
                 
             })
             output$distribution_display = renderPlot({
+                
             })
         }
         else if (input$Test == 4) {
+            #Single Sample Z-Test
+            #Create Data
+            data = data.frame(
+                X = sample(
+                    seq(input$value_range[1], input$value_range[2], by = .1),
+                    1
+                ),
+                mu = sample(
+                    seq(input$value_range[1], input$value_range[2], by = .1),
+                    1
+                ),
+                sigma = rnorm(1, (
+                    input$value_range[2] - input$value_range[1]
+                ) / 5, .1)
+            )
+            data2 = data.frame(data = dnorm(
+                seq((data$mu - (
+                    4 * data$sigma
+                )), (data$mu + (
+                    4 * data$sigma
+                )), length.out = 100),
+                mean = data$mu,
+                sd = data$sigma
+            ))
+            plotdata$data = data2
+            
+            #Create Table
+            descriptives = data.frame(
+                Z_Value = (data$X - data$mu) / data$sigma,
+                P_Value_of_X_and_Below = round(pnorm(
+                    round((data$X - data$mu) / data$sigma, digits = 2)
+                ), digits = 4),
+                P_Value_of_X_and_Above = round(pnorm(
+                    round((data$X - data$mu) / data$sigma, digits = 2), lower.tail = F
+                ), digits = 4)
+            )
+            #Set Outputs
+            stats$data_table = descriptives
+            output$data_display = renderRHandsontable(rhandsontable(as.data.frame(t(data))))
+            output$stats_display = renderRHandsontable({
+                
+            })
+            output$distribution_display = renderPlot({
+                
+            })
+        }
+        else if (input$Test == 5) {
             #Single Sample T-Test
             #Create Data
             data = data.frame(
-                Data = sample(input$value_range[1]:input$value_range[2], input$num_of_participants, replace = TRUE),
+                Data = sample(
+                    input$value_range[1]:input$value_range[2],
+                    input$num_of_participants,
+                    replace = TRUE
+                ),
                 mu = c(
                     sample(1:20, 1),
                     rep(NA, input$num_of_participants - 1)
@@ -218,6 +309,7 @@ server = function(input, output) {
                 
             })
             output$distribution_display = renderPlot({
+                
             })
         } else {
             #Repeated Measures T-Test
@@ -259,23 +351,30 @@ server = function(input, output) {
     })
     observeEvent(input$answers,
                  {
-                     output$stats_display = renderRHandsontable(rhandsontable(stats$data_table)%>%
-                                                                    hot_col("P_Value_of_X_and_Below", format = "0.0000")%>%
-                                                                    hot_col("P_Value_of_X_and_Above", format = "0.0000")) 
+                     output$stats_display = renderRHandsontable(if (input$Test == 3) {
+                         rhandsontable(stats$data_table) %>%
+                             hot_col("P_Value_of_X_and_Below", format = "0.0000") %>%
+                             hot_col("P_Value_of_X_and_Above", format = "0.0000")
+                     }
+                     else{
+                         rhandsontable(stats$data_table)
+                     })
                  })
     observeEvent(input$distribution,
                  {
-                     output$distribution_display = renderPlot(
-                         if (input$Test == 3){
-                                ggplot(aes(x = 1:100,y = data), data = plotdata$data) +
-                                 geom_line()+
-                                 theme_void()
-                             }else{
-                                 ggplot(aes(x = data), data = plotdata$data) +
-                             geom_histogram(color = "#E27D60", fill = "#E8A87C",binwidth = 1) +
+                     output$distribution_display = renderPlot(if (input$Test == 3) {
+                         ggplot(aes(x = 1:100, y = data), data = plotdata$data) +
+                             geom_line() +
+                             theme_void()
+                     } else{
+                         ggplot(aes(x = data), data = plotdata$data) +
+                             geom_histogram(color = "#E27D60",
+                                            fill = "#E8A87C",
+                                            binwidth = 1) +
                              scale_x_continuous(
                                  breaks = 1:input$value_range[2],
-                                 limits = c(input$value_range[1]-1, input$value_range[2]+1),
+                                 limits = c(input$value_range[1] - 1, input$value_range[2] +
+                                                1),
                                  name = 'X Values'
                              ) +
                              ylab('Frequency Count') +
