b274dad1e3c1d85d76cf19ecfe88caeffcaac969
chadcwilliams <ccwillia@uvic.ca>
2021-06-15 20:45:33 -0700

    Added single sample z-test & t-test

--- StatsProblemGenerator/app.R
+++ StatsProblemGenerator/app.R
@@ -21,10 +21,10 @@ ui = fluidPage(tags$head(tags$style(type = "text/css", ".irs {max-width: 946px;}
                            choices = list(
                                "Frequency Distribution" = 1,
                                "Descriptives" = 2,
-                               "Single Participant Z-Test" = 3,
                                "Correlation & Regression" = 4,
-                               "Single Sample T-Test" = 4,
-                               "Paired Sample T-Test" = 4
+                               "Single Participant Z-Test" = 3,
+                               "Single Sample Z-Test" = 5,
+                               "Single Sample T-Test" = 6
                            ),
                            selected = 1
                        ),
@@ -198,7 +198,15 @@ server = function(input, output) {
                     input$value_range[2] - input$value_range[1]
                 ) / 5, .1)
             )
-            data2 = data.frame(data = dnorm(seq((data$mu - (4 * data$sigma)), (data$mu + (4 * data$sigma)), length.out = 100),mean = data$mu,sd = data$sigma))
+            data2 = data.frame(data = dnorm(
+                seq((data$mu - (
+                    4 * data$sigma
+                )), (data$mu + (
+                    4 * data$sigma
+                )), length.out = 100),
+                mean = data$mu,
+                sd = data$sigma
+            ))
             plotdata$data = data2
             
             #Create Table
@@ -224,38 +232,64 @@ server = function(input, output) {
         else if (input$Test == 4) {
             #Correlation & Regression
             #Create Data
-            data = rnorm_multi(n=input$num_of_participants,
-                        mu = c(sample(input$value_range[1]:input$value_range[2],1),sample(input$value_range[1]:input$value_range[2],1)),
-                        sd = c(rnorm(1, (
-                            input$value_range[2] - input$value_range[1]
-                        ) / 5, .1),rnorm(1, (
-                            input$value_range[2] - input$value_range[1]
-                        ) / 5, .1)),
-                        r = sample(seq(-1,1,.01),1),
-                        varnames = c('X','Y'))
+            data = rnorm_multi(
+                n = input$num_of_participants,
+                mu = c(
+                    sample(input$value_range[1]:input$value_range[2], 1),
+                    sample(input$value_range[1]:input$value_range[2], 1)
+                ),
+                sd = c(rnorm(
+                    1, (input$value_range[2] - input$value_range[1]) / 5, .1
+                ), rnorm(
+                    1, (input$value_range[2] - input$value_range[1]) / 5, .1
+                )),
+                r = sample(seq(-1, 1, .01), 1),
+                varnames = c('X', 'Y')
+            )
             data$X = as.integer(data$X)
             data$Y = as.integer(data$Y)
-            data$X_SD = c(sqrt(sum((data$X-mean(data$X))^2)/dim(data)[1]),rep(NA,dim(data)[1]-1))
-            data$Y_SD = c(sqrt(sum((data$Y-mean(data$Y))^2)/dim(data)[1]),rep(NA,dim(data)[1]-1))
+            data$X_SD = c(sqrt(sum((
+                data$X - mean(data$X)
+            ) ^ 2) / dim(data)[1]), rep(NA, dim(data)[1] - 1))
+            data$Y_SD = c(sqrt(sum((
+                data$Y - mean(data$Y)
+            ) ^ 2) / dim(data)[1]), rep(NA, dim(data)[1] - 1))
             plotdata$data = data
             
             #Create Table
             descriptives = data.frame(
                 X_Mean = mean(data$X),
-                X_SD = sqrt(sum((data$X-mean(data$X))^2)/dim(data)[1]),
+                X_SD = sqrt(sum((
+                    data$X - mean(data$X)
+                ) ^ 2) / dim(data)[1]),
                 Y_Mean = mean(data$Y),
-                Y_SD = sqrt(sum((data$Y-mean(data$Y))^2)/dim(data)[1]),
-                SP = sum((data$X-mean(data$X))*(data$Y-mean(data$Y))),
-                COV = sum((data$X-mean(data$X))*(data$Y-mean(data$Y)))/dim(data)[1],
-                r = cor(data$X,data$Y)
+                Y_SD = sqrt(sum((
+                    data$Y - mean(data$Y)
+                ) ^ 2) / dim(data)[1]),
+                SP = sum((data$X - mean(data$X)) * (data$Y - mean(data$Y))),
+                COV = sum((data$X - mean(data$X)) * (data$Y - mean(data$Y))) /
+                    dim(data)[1],
+                r = cor(data$X, data$Y)
             )
-            descriptives$by = descriptives$r*(descriptives$Y_SD/descriptives$X_SD)
-            descriptives$ay = descriptives$Y_Mean - (round(descriptives$by,4)*descriptives$X_Mean)
-            descriptives$bx = descriptives$r*(descriptives$X_SD/descriptives$Y_SD)
-            descriptives$ax = descriptives$X_Mean - (round(descriptives$bx,4)*descriptives$Y_Mean)
-            descriptives$SD_XPrime = round((round(descriptives$X_SD,2)*(round(sqrt(1-(abs(round(cor(data$X,data$Y),4))^2)),4))),2)
-            descriptives$SD_Yprime = round((round(descriptives$Y_SD,2)*(round(sqrt(1-(abs(round(cor(data$X,data$Y),4))^2)),4))),2)
-
+            descriptives$by = descriptives$r * (descriptives$Y_SD / descriptives$X_SD)
+            descriptives$ay = descriptives$Y_Mean - (round(descriptives$by, 4) *
+                                                         descriptives$X_Mean)
+            descriptives$bx = descriptives$r * (descriptives$X_SD / descriptives$Y_SD)
+            descriptives$ax = descriptives$X_Mean - (round(descriptives$bx, 4) *
+                                                         descriptives$Y_Mean)
+            descriptives$SD_XPrime = round((round(descriptives$X_SD, 2) *
+                                                (round(
+                                                    sqrt(1 - (abs(
+                                                        round(cor(data$X, data$Y), 4)
+                                                    ) ^ 2)), 4
+                                                ))), 2)
+            descriptives$SD_Yprime = round((round(descriptives$Y_SD, 2) *
+                                                (round(
+                                                    sqrt(1 - (abs(
+                                                        round(cor(data$X, data$Y), 4)
+                                                    ) ^ 2)), 4
+                                                ))), 2)
+            
             #Set Outputs
             stats$data_table = descriptives
             output$data_display = renderRHandsontable(rhandsontable(as.data.frame(data)))
@@ -267,7 +301,7 @@ server = function(input, output) {
             })
         }
         else if (input$Test == 5) {
-            #Single Sample T-Test
+            #Single Sample Z-Test
             #Create Data
             data = data.frame(
                 Data = sample(
@@ -275,71 +309,185 @@ server = function(input, output) {
                     input$num_of_participants,
                     replace = TRUE
                 ),
+                n = input$num_of_participants,
                 mu = c(
                     sample(1:20, 1),
                     rep(NA, input$num_of_participants - 1)
                 )
             )
-            data$mu[1] = as.integer(round(rnorm(
+            data$mu[1] = (round(rnorm(
                 1, mean(data$Data), sd(data$Data) / 5
             )))
+            data$sigma = c((round(runif(1, 0.5, 2.5),2)),
+                           rep(NA, input$num_of_participants - 1))
+            dir = runif(1)
+            if (dir < .5) {
+                data$direction = c('Two-Tail',
+                                   rep(NA, input$num_of_participants - 1))
+                direction = 1
+            } else if (dir < .75) {
+                data$direction = c('One-Tail (lower)',
+                                   rep(NA, input$num_of_participants - 1))
+                direction = 2
+            }else{
+                data$direction = c('One-Tail (higher)',
+                                   rep(NA, input$num_of_participants - 1))
+                direction = 3
+            }
+            
+            data$p_alpha = c(.05, rep(NA, input$num_of_participants - 1))
+            data$X_Mean = c(mean(data$Data),
+                            rep(NA, input$num_of_participants - 1))
             plotdata$data = as.data.frame(data$Data)
             #Create Stats
-            t = t.test(data$Data, mu = data$mu[1])
             descriptives = data_table = data.frame(
-                Mean = mean(data$Data),
-                Variance = var(data$Data),
-                SD = sd(data$Data),
-                T_Value = round(as.numeric(t["statistic"]), digits = 2),
-                Degrees_of_Freedom = as.numeric(input$num_of_participants - 1),
-                P_Value = round(as.numeric(t["p.value"]), digits = 4)
+                SE = data$mu[1] / sqrt(input$num_of_participants),
+                z_Obs = (data$X_Mean[1] - data$mu[1]) / (data$sigma[1] /
+                                                             sqrt(input$num_of_participants)),
+                z_Crit = if (direction == 1) {
+                    '+-1.96'
+                } else if (direction == 2) {
+                    '-1.645'
+                } else{
+                    '+1.645'
+                }
             )
+            descriptives$p_obs = pnorm(round((data$X_Mean[1] - data$mu[1]) / (data$sigma[1] / sqrt(input$num_of_participants)),4))
+            if (descriptives$z_Obs>=0){descriptives$p_obs=1-descriptives$p_obs}
+            if (direction == 1) {descriptives$p_obs=descriptives$p_obs*2}
+                
+            descriptives$p_alpha = .05
+            if (direction == 1){
+                descriptives$H0 = if (descriptives$p_obs < .05){'Reject'}else{'Retain'}
+                descriptives$H1 = if (descriptives$p_obs < .05){'Accept'}else{'Suspend'}}
+            else if (direction == 2){
+                if (descriptives$z_Obs<0){
+                    descriptives$H0 = if (descriptives$p_obs < .05){'Reject'}else{'Retain'}
+                    descriptives$H1 = if (descriptives$p_obs < .05){'Accept'}else{'Suspend'}}
+                else{
+                    descriptives$H0 = 'Retain'
+                    descriptives$H1 = 'Suspend'}
+            } else {
+                if (descriptives$z_Obs>0){
+                    descriptives$H0 = if (descriptives$p_obs < .05){'Reject'}else{'Retain'}
+                    descriptives$H1 = if (descriptives$p_obs < .05){'Accept'}else{'Suspend'}}
+                else{
+                    descriptives$H0 = 'Retain'
+                    descriptives$H1 = 'Suspend'}
+            }
+            
+            
             #Set Outputs
             stats$data_table = descriptives
-            output$data_display = renderRHandsontable(rhandsontable(as.data.frame(data)))
+            output$data_display = renderRHandsontable(rhandsontable(as.data.frame(t(data[1, 2:dim(data)[2]]))))
+            output$stats_display = renderRHandsontable({
+                
+            })
+           output$distribution_display = renderPlot({
+                
+            })
+        } else {
+            #Single Sample Z-Test
+            #Create Data
+            data = data.frame(
+                Data = sample(
+                    input$value_range[1]:input$value_range[2],
+                    input$num_of_participants,
+                    replace = TRUE
+                ),
+                n = input$num_of_participants,
+                mu = c(
+                    sample(1:20, 1),
+                    rep(NA, input$num_of_participants - 1)
+                )
+            )
+            data$mu[1] = (round(rnorm(
+                1, mean(data$Data), sd(data$Data) / 2.5
+            )))
+            data$SS = sum(((data$Data - mean(data$Data))^2))
+            dir = runif(1)
+            if (dir < .5) {
+                data$direction = c('Two-Tail',
+                                   rep(NA, input$num_of_participants - 1))
+                direction = 1
+            } else if (dir < .75) {
+                data$direction = c('One-Tail (lower)',
+                                   rep(NA, input$num_of_participants - 1))
+                direction = 2
+            }
+            else{
+                data$direction = c('One-Tail (higher)',
+                                   rep(NA, input$num_of_participants - 1))
+                direction = 3
+            }
+            
+            data$p_alpha = c(.05, rep(NA, input$num_of_participants - 1))
+            data$X_Mean = c(mean(data$Data),
+                            rep(NA, input$num_of_participants - 1))
+            plotdata$data = as.data.frame(data$Data)
+            #Create Stats
+            t = t.test(data$Data,mu=data$mu[1])
+            descriptives = data_table = data.frame(
+                SS = sum(((data$Data - mean(data$Data))^2)),
+                s = sd(data$Data),
+                SE = sd(data$Data) / sqrt(input$num_of_participants),
+                df = input$num_of_participants-1,
+                t_Obs = as.numeric(t[['statistic']]),
+                t_Crit = 
+                if (direction == 1) {
+                    paste('+-',toString(round(qt(p=.975, df=input$num_of_participants-1),2)))
+                } else if (direction == 2) {
+                    round(qt(p=.05, df=input$num_of_participants-1),2)
+                } else{
+                    round(qt(p=.95, df=input$num_of_participants-1),2)
+                }
+            )
+            
+            descriptives$p_obs = as.numeric(t[['p.value']])
+            descriptives$p_alpha = .05
+            if (direction == 1){
+                descriptives$H0 = if (descriptives$p_obs < .05){'Reject'}else{'Retain'}
+                descriptives$H1 = if (descriptives$p_obs < .05){'Accept'}else{'Suspend'}}
+            else if (direction == 2){
+                if (descriptives$t_Obs<0){
+                    descriptives$H0 = if (descriptives$p_obs < .05){'Reject'}else{'Retain'}
+                    descriptives$H1 = if (descriptives$p_obs < .05){'Accept'}else{'Suspend'}}
+                else{
+                    descriptives$H0 = 'Retain'
+                    descriptives$H1 = 'Suspend'}
+            } else {
+                if (descriptives$t_Obs>0){
+                    descriptives$H0 = if (descriptives$p_obs < .05){'Reject'}else{'Retain'}
+                    descriptives$H1 = if (descriptives$p_obs < .05){'Accept'}else{'Suspend'}}
+                else{
+                    descriptives$H0 = 'Retain'
+                    descriptives$H1 = 'Suspend'}
+            }
+            
+            
+            #Set Outputs
+            stats$data_table = descriptives
+            output$data_display = renderRHandsontable(rhandsontable(as.data.frame(t(data[1, 2:dim(data)[2]]))))
             output$stats_display = renderRHandsontable({
                 
             })
             output$distribution_display = renderPlot({
                 
             })
-        } else {
-            #Repeated Measures T-Test
-            run = 1
-            while (run == 1) {
-                data1 = sample(1:20, input$num_of_participants, replace = TRUE)
-                data2 = sample(1:20, input$num_of_participants, replace = TRUE)
-                M1 = mean(data1)
-                V1 = var(data1)
-                SD1 = sd(data1)
-                M2 = mean(data2)
-                V2 = var(data2)
-                SD2 = sd(data2)
-                t = t.test(data1, data2, paired = TRUE)
-                if (input$decimal == 2) {
-                    if (M1 %% 1 == 0 &
-                        V1 %% 1 == 0 &
-                        SD1 %% 1 == 0 &
-                        M2 %% 1 == 0 &
-                        V2 %% 1 == 0 &
-                        SD2 %% 1 == 0) {
-                        print(paste("Group 1 Data: ", toString(data1)))
-                        print(paste("Group 2 Data: ", toString(data2)))
-                        output$stats_display = renderRHandsontable({
-                            
-                        })
-                        run = 0
-                    }
-                } else{
-                    print(paste("Group 1 Data: ", toString(data1)))
-                    print(paste("Group 2 Data: ", toString(data2)))
-                    output$stats_display = renderRHandsontable({
-                        
-                    })
-                    run = 0
-                }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             }
-        }
     })
     observeEvent(input$answers,
                  {
@@ -356,20 +504,28 @@ server = function(input, output) {
                  {
                      output$distribution_display = renderPlot(if (input$Test == 3) {
                          ggplot(aes(x = 1:100, y = data), data = plotdata$data) +
-                             geom_line()+
-                             geom_vline(xintercept=round((stats$data_table$P_Value_of_X_and_Below*100))+.5,color='red')+
+                             geom_line() +
+                             geom_vline(xintercept = round((
+                                 stats$data_table$P_Value_of_X_and_Below * 100
+                             )) + .5, color = 'red') +
                              theme_void()
-                     } else if (input$Test == 4){
-                         ggplot(aes(x=X,y=Y),data = plotdata$data)+
-                             geom_point(size=4,alpha=.5)+
-                             geom_smooth(method=lm,se=F)+
-                             geom_segment(y=min(plotdata$data$Y),
-                                          x=(stats$data_table$ax + (stats$data_table$bx*min(plotdata$data$Y))),
-                                          yend=max(plotdata$data$Y),
-                                          xend=(stats$data_table$ax + (stats$data_table$bx*max(plotdata$data$Y))),
-                                          color='red')+
-                             theme_classic()+
-                             theme(text = element_text(size=20))
+                     } else if (input$Test == 4) {
+                         ggplot(aes(x = X, y = Y), data = plotdata$data) +
+                             geom_point(size = 4, alpha = .5) +
+                             geom_smooth(method = lm, se = F) +
+                             geom_segment(
+                                 y = min(plotdata$data$Y),
+                                 x = (stats$data_table$ax + (
+                                     stats$data_table$bx * min(plotdata$data$Y)
+                                 )),
+                                 yend = max(plotdata$data$Y),
+                                 xend = (stats$data_table$ax + (
+                                     stats$data_table$bx * max(plotdata$data$Y)
+                                 )),
+                                 color = 'red'
+                             ) +
+                             theme_classic() +
+                             theme(text = element_text(size = 20))
                      }
                      else{
                          ggplot(aes(x = data), data = plotdata$data) +
